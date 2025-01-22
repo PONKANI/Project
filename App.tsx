5068db97c34320db8522480cb159deb85c871eaf
PONKANI <ponkanicse@gmail.com>
2025-01-22 13:31:57 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,203 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
-
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-
+// import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import tw from 'twrnc';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View style={tw `p-10`}>
     
      
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>Set Password</Text>
-        <Text style={styles.subText}>Enter your new password</Text>
+      <View style={tw `mt-20 justify-center items-center`}>
+        <Text style={tw `font-bold text-2xl`}>Set Password</Text>
+        <Text style={tw `text-gray-500`}>Enter your new password</Text>
+        {/* <AntDesignIcon name={"facebook-square"} size={30} color="black" /> */}
       </View>
 
-      <View style={styles.formContainer}>
-        <Text style={styles.label}>password</Text>
-        <TextInput
+      <View style={tw `mt-20`}>
+        <Text style={tw `text-gray-500`}>Password</Text>
+        <TextInput  
           placeholder="Enter your full name"
-          style={styles.input}
+          style={tw `text-gray-500 mt-3 p-3 border-gray-100 bg-gray-100 rounded-lg`} 
         />
 
-        <Text style={styles.label}>Retype Password</Text>
+        <Text style={tw `text-gray-500 mt-1`}>Retype Password</Text>
         <TextInput
           placeholder="Enter the password"
-          style={styles.input}
+          style={tw `text-gray-500 mt-3 p-3 border-gray-100 bg-gray-100 rounded-lg`}
         />
 
        
       </View>
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Set Password</Text>
+      <TouchableOpacity style={tw `bg-blue-500 mt-30 p-3 rounded-lg`}>
+        <Text style={tw ` text-white text-2xl text-center`} >Set Password</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 40,
-  },
-
-  headerContainer: {
-    marginTop: 90,
-    alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  subText: {
-    color: 'gray',
-  },
-  formContainer: {
-    marginTop: 70,
-  },
-  label: {
-    color: 'gray',
-    marginBottom: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ddd',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 15,
-    backgroundColor: '#f7f7f7',
-  },
-  button: {
-    backgroundColor: '#007bff',
-    paddingVertical: 12,
-    borderRadius: 5,
-    marginTop: 90,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-  },
-});
 
 export default App;
